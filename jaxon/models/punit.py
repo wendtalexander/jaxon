580f6452087a879c0e4a200d39a8221a5b2f2a3d
wendtalexander <wendtalexander@protonmail.com>
2025-09-27 09:10:45 +0200

    [models] updating doc punit

--- jaxon/models/punit.py
+++ jaxon/models/punit.py
@@ -4,11 +4,13 @@ import jax
 import jax.numpy as jnp
 from jax.typing import ArrayLike
 
+from jaxon.utils.output import SIMOutput
+
 
 @jax.tree_util.register_dataclass
 @dataclass
 class PUnitParams:
-    """Parameter class for p-unit model
+    """Parameter class for P-unit model.
 
     Attributes
     ----------
@@ -55,14 +57,30 @@ class PUnitParams:
     v_zero: float
 
 
-def simulate(key: ArrayLike, stimulus: ArrayLike, params: PUnitParams) -> jax.Array:
-    """Simulate a P-unit using JAX, returning a binary spike train."""
+def simulate(key: ArrayLike, stimulus: ArrayLike, params: PUnitParams) -> SIMOutput:
+    """Simulate a P-unit.
+
+    Parameters
+    ----------
+    key : jax.random.PRNGKey
+        Random key for noise generation.
+    stimulus : 1-D jnp.ndarray
+        Input stimulus.
+    params: PUnitParams
+        Parameter class
+
+    Returns
+    -------
+    SIMOutput: class
+        Simulation output class which holds both binary spikes and membrane voltage
+
+    """
     noise = jax.random.normal(key, shape=stimulus.shape)
     noise *= params.noise_strength / jnp.sqrt(params.deltat)
 
     stimulus = jnp.maximum(stimulus, 0.0)
 
-    def _scan_body(carry, x):
+    def step(carry, x):
         v_mem, v_dend, adapt, last_spike_time, time_index = carry
         stim_i, noise_i = x
         current_time = time_index * params.deltat
@@ -99,9 +117,7 @@ def simulate(key: ArrayLike, stimulus: ArrayLike, params: PUnitParams) -> jax.Ar
 
         # Apply spike effects conditionally using jnp.where
         final_v_mem = jnp.where(spike_fired, params.v_base, v_mem_ref)
-        final_adapt = jnp.where(
-            spike_fired, adapt_new + params.delta_a / params.tau_a, adapt_new
-        )
+        final_adapt = jnp.where(spike_fired, adapt_new + params.delta_a / params.tau_a, adapt_new)
         final_last_spike_time = jnp.where(spike_fired, current_time, last_spike_time)
 
         new_carry = (
@@ -111,12 +127,12 @@ def simulate(key: ArrayLike, stimulus: ArrayLike, params: PUnitParams) -> jax.Ar
             final_last_spike_time,
             time_index + 1,
         )
-        return new_carry, spike_fired
+        return new_carry, (spike_fired, final_v_mem)
 
     # Initial conditions for the scan
     initial_carry = (params.v_zero, stimulus[0], params.a_zero, -1.0, 0)
 
     # Run the simulation using jax.lax.scan
-    _, spike_fired_series = jax.lax.scan(_scan_body, initial_carry, (stimulus, noise))
+    _, (spikes, vmem) = jax.lax.scan(step, initial_carry, (stimulus, noise))
 
-    return spike_fired_series.astype(jnp.int32)
+    return SIMOutput(spikes.astype(jnp.int32), vmem)
