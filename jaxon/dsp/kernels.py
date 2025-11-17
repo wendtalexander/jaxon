1e9029810b8ab3a87e2ee83d3b08046b40f5fb5c
wendtalexander <wendtalexander@protonmail.com>
2025-11-17 16:30:42 +0100

    [dsp] renaming

--- jaxon/dsp/kernels.py
+++ jaxon/dsp/kernels.py
@@ -1,6 +1,6 @@
 import jax.numpy as jnp
 
 
-def gauss_kernel_jax(sigma, dt, k_time):
+def gauss_kernel(sigma, dt, k_time):
     x = jnp.arange(-k_time * sigma, k_time * sigma, dt)
     return jnp.exp(-0.5 * (x / sigma) ** 2) / jnp.sqrt(2.0 * jnp.pi) / sigma
