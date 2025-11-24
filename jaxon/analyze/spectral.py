6a37f3e098500a4ff0e131e7d0939812074e9696
wendtalexander <wendtalexander@protonmail.com>
2025-11-24 09:21:22 +0100

    [analyze] adding doc to spectral

--- jaxon/analyze/spectral.py
+++ jaxon/analyze/spectral.py
@@ -7,7 +7,33 @@ import numpy as np
 from jax.typing import ArrayLike
 
 
-def spectra(spikes: ArrayLike, stimulus: ArrayLike, fs: int, nfft: int):
+def spectra(
+    spikes: ArrayLike, stimulus: ArrayLike, fs: int, nfft: int
+) -> tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+    """Calculate spectral properties of the given spikes and stimulus.
+
+    Uses the jax scipy welch method
+
+    Parameters
+    ----------
+    spikes : ArrayLike
+        Binaray spike train
+
+    stimulus : ArrayLike
+        Stimulus array
+
+    fs : int
+        Sampling frequency in Hertz like 30_000 Hz
+
+    nfft : int
+        Window size for the fast Fourier transfomation
+
+    Returns
+    -------
+    f, pyy, pxx, pxy: jnp.ndarray
+        frequency, stimulus power spectra, spike power spectra and cross spectra
+
+    """
     f, pyy = jsp.signal.welch(stimulus, fs=fs, nperseg=nfft, noverlap=nfft // 2)
     _, pxx = jsp.signal.welch(spikes - jnp.mean(spikes), fs=fs, nperseg=nfft, noverlap=nfft // 2)
     _, pxy = jsp.signal.csd(
